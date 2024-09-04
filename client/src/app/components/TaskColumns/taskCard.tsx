6279bd917221618d25ce6e3d5ffc7e39400c06d3
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-09-05 01:51:44 +0530

    On click of edit updated the create ticket form state using redux

--- client/src/app/components/TaskColumns/taskCard.tsx
+++ client/src/app/components/TaskColumns/taskCard.tsx
@@ -1,5 +1,8 @@
 import { format, formatDistanceToNowStrict } from "date-fns"
 import { Priority, Status } from "./taskColumn"
+import { useDispatch } from "react-redux"
+import { setDeadline, setDescription, setPriority, setStatus, setTitle } from "@/app/redux/slice/taskState.slice"
+import { setCreateDrawerVisibility } from "@/app/redux/slice/usernameSlice"
 
 interface TaskCardProps {
     title: string,
@@ -13,12 +16,27 @@ interface TaskCardProps {
 }
 
 export default function TaskCard({title, description, status, priority, deadline, createdAt, index, setactiveCard}: TaskCardProps) {
+    const dispatch = useDispatch();
     let timeAgo = formatDistanceToNowStrict(new Date(createdAt), {addSuffix: true});
     const formatedDate = format(new Date(deadline), 'yyyy-MM-dd')
 
+    function handleEditClick() {
+        dispatch(setTitle(title));
+        dispatch(setStatus(status));
+        dispatch(setPriority(priority));
+        dispatch(setDeadline(deadline));
+        dispatch(setDescription(description));
+        dispatch(setCreateDrawerVisibility(true))
+    }
+
     return (
     <div className="flex flex-col p-[14px] bg-[#F9F9F9] rounded-lg border-[#DEDEDE] border text-[#606060] cursor-grab active:opacity-70 active:border active:border-[#797979]" draggable onDragStart={() => setactiveCard(index)} onDragEnd={() => setactiveCard(null)}>
-        <h3 className="text-[16px] font-semibold">{title}</h3>
+        <div className="flex justify-between items-start">
+            <h3 className="text-[16px] font-semibold">{title}</h3>
+            <div onClick={handleEditClick} className="cursor-pointer box-border p-1 hover:bg-white hover:border-[#DEDEDE] hover:border transition-all duration-150">
+                <img className="w-full h-full" src="/ticketsidebar/description-icon.svg" alt="edit-buton" />
+            </div>
+        </div>
         <p className="text-[14px] text-[#797979] mt-1">{description}</p>
         <span className={`text-[12px] mt-[13px] px-2 py-[6px] inline-block max-w-fit text-white rounded-lg ${priority==='urgent' ? 'bg-[#FF6B6B]' : ''} || ${priority==='high' ? 'bg-[#b62b2b]' : ''} || ${priority==='medium' ? 'bg-[#FFA235]' : ''} || ${priority==='low' ? 'bg-[#0ECC5A]' : ''}`}>{priority}</span>
         <div className="flex items-center gap-x-2 mt-[13px]">
