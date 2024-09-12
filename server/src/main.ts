1265da5fbbdc858ec6aa712c0150da9386dc8319
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-09-12 18:18:37 +0530

    Fixed the cors orgin and port

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,20 +8,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000'],
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+
+  const PORT = process.env.PORT
     
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(8080);
+  await app.listen(PORT);
 }
 bootstrap();
