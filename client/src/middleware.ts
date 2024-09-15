88af0c902eaec4f65af7da0251898dbeb01e5d79
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-09-15 20:28:51 +0530

    debugging

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -8,6 +8,8 @@ export function middleware(request: NextRequest) {
 
     let token = request.cookies.get('token');
 
+    console.log(token);
+
     if(!token) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
