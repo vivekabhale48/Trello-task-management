2d4b55ed9c3237b2993bc563f98ee35f197936e3
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-09-19 08:50:08 +0530

    .

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser())
+  app.set("trust proxy", 1)
   console.log('Client URL:', process.env.CLIENT_URL);
-
+  
   app.enableCors({
     origin: [
       'http://localhost:3000', 
