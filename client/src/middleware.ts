946432724280fc125b8895363ca5d70fea7120aa
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-09-22 01:07:45 +0530

    updated the middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-    matcher: ['/'], // Apply middleware to protected routes
+    matcher: "/:path*",
 };
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 
     let token = request.cookies.get('token');
 
     console.log(token);
 
-    if(!token) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+    // if(!token) {
+    //     return NextResponse.redirect(new URL('/login', request.url))
+    // }
 
     return NextResponse.next()
 }
